dc4b94b4ee7ba77ec29e7a68e7519830d07709f2
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-11 03:49:25 +0900

    CORS設定&api疎通確認

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -3,7 +3,7 @@ import useSWR from 'swr'
 import { fetcher } from '@/utils'
 
 const Index: NextPage = () => {
-  const url = 'http://localhost:3000/api/v1/health_check'
+  const url = 'http://localhost:3001/api/v1/health_check'
   const { data, error } = useSWR(url, fetcher)
 
   if (error) return <div>An error has occurred.</div>
