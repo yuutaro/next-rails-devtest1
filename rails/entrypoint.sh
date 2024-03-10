58993454ce9ece1b72713a4ed00d026ac46b1cac
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-11 03:48:27 +0900

    rails/entrypoint.sh修正

--- rails/entrypoint.sh
+++ rails/entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 rm -f /myapp/tmp/pids/server.pid
 
-USERID=${LOCAL_USER_ID :- 1000}
-GROUPID=${LOCAL_GROUP_ID :- 1000}
+USERID=${LOCAL_USER_ID:-1000}
+GROUPID=${LOCAL_GROUP_ID:-1000}
 
 echo "UserName: dockeruser, UserID: $USERID, GroupID: $GROUPID"
 groupadd -g $GROUPID dockeruser
