8a5378151d3b5223cf9864260ca99d16adc49f6a
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-10 19:06:47 +0900

    rubocop修正

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -21,6 +21,7 @@ require 'action_view/railtie'
 Bundler.require(*Rails.groups)
 
 module Myapp
+  # Application
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
