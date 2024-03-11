29a2f40b43bc12f6a297e45d06a47c4670eaf8d0
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-11 15:47:00 +0900

    Userモデルモデル作成

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount_devise_token_auth_for "User", at: "auth"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -10,6 +9,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "health_check", to: "health_check#index"
+      mount_devise_token_auth_for "User", at: "auth"
     end
   end
 end
