48f8883fd4280178b19066866d1b91d7bd6003ea
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-13 20:08:53 +0900

    api/vi/usersコントローラー作成

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       get "health_check", to: "health_check#index"
       mount_devise_token_auth_for "User", at: "auth"
+
+      namespace :current do
+        resource :user, only: %i[show]
+      end
     end
   end
 end
