1dac392816211189591032b4dafa4f076902f7ab
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-14 02:34:20 +0900

    active_model_serializers導入

--- rails/app/controllers/api/v1/current/users_controller.rb
+++ rails/app/controllers/api/v1/current/users_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::Current::UsersController < Api::V1::BaseController
   before_action :authenticate_user!
 
   def show
-    render json: current_user
+    render json: current_user, serializer: CurrentUserSerializer
   end
 end
