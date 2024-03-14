38b5bbd28c763a09391abd40c2eee47bacfb08a6
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-15 02:41:58 +0900

    articlesモデルテスト追加

--- rails/app/models/article.rb
+++ rails/app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
-  enum status: { unsaved: 10, draft: 20, published: 30 }, _prefix: true
+  enum :status, { unsaved: 10, draft: 20, published: 30 }, _prefix: true
   validates :title, :content, presence: true, if: :published?
+  validate :verify_only_unsaved_status_is_allowed
 
   private
 
