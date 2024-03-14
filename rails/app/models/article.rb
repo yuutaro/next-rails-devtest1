deee75b509b38d0a1b6bef7103b1e53d3bdece55
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-15 02:19:49 +0900

    Articleモデルバリデーション追加

--- rails/app/models/article.rb
+++ rails/app/models/article.rb
@@ -1,3 +1,13 @@
 class Article < ApplicationRecord
   belongs_to :user
+  enum status: { unsaved: 10, draft: 20, published: 30 }, _prefix: true
+  validates :title, :content, presence: true, if: :published?
+
+  private
+
+    def verify_only_unsaved_status_is_allowed
+      if unsaved? && user.articles.unsaved.present?
+        raise StandardError, "未保存の記事は複数保有できません"
+      end
+    end
 end
