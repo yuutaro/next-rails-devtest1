fb51babe85ca4db87b7b4d2338aff2116d4c861f
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-15 01:52:47 +0900

    Articleモデル作成

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :articles, dependent: :destroy
 end
