d4a9d730fc1db6ec336b78040750354b1b9914e9
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-15 15:41:21 +0900

    Articles/indexアクション　N+1問題修正

--- rails/app/controllers/api/v1/articles_controller.rb
+++ rails/app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ArticlesController < Api::V1::BaseController
   def index
-    articles = Article.published.order(created_at: :desc).page(params[:page] || 1).per(params[:per_page] || 10)
+    articles = Article.published.order(created_at: :desc).page(params[:page] || 1).per(params[:per_page] || 10).includes(:user)
     render json: articles
   end
 
