0c3c6ba097c3e4933714063c2559dcb88c89a162
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-15 15:54:42 +0900

    ページネーション　総ページ数、現在のページのメタデータ付与

--- rails/app/controllers/api/v1/articles_controller.rb
+++ rails/app/controllers/api/v1/articles_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::ArticlesController < Api::V1::BaseController
+  include Pagination
+
   def index
     articles = Article.published.order(created_at: :desc).page(params[:page] || 1).per(params[:per_page] || 10).includes(:user)
-    render json: articles
+    render json: articles, meta: pagination(articles), adapter: :json
   end
 
   def show
