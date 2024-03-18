5c8e67c4ad16b62e68615dc49498122968d45696
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-19 01:29:56 +0900

    axios, swrインストール & railsとの疎通確認

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import type { NextPage } from 'next'
+import useSWR from 'swr'
+import { fetcher } from '@/utils'
+
+const Index: NextPage = () => {
+  const url = 'http://localhost:3001/api/v1/health_check'
+  const { data, error } = useSWR(url, fetcher)
+
+  if (error) return <div>An error has occurred.</div>
+  if (!data) return <div>Loading...</div>
+
   return (
     <>
-      <div>hello world</div>
+      <div>Rails疎通確認</div>
+      <div>レスポンスメッセージ: {data.message}</div>
     </>
   )
 }
+
+export default Index
