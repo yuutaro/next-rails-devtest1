c6ccb04651cd62b21573cc1af38ff8b0a8a4be72
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-19 02:10:58 +0900

    Header実装

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from '@mui/material/styles'
 import { AppProps } from 'next/app'
 import * as React from 'react'
 import '@/styles/destyle.css'
-
+import Header from '@/components/Header'
 import createEmotionCache from '@/styles/createEmotionCache'
 import theme from '@/styles/theme'
 
@@ -22,6 +22,7 @@ export default function MyApp(props: MyAppProps): JSX.Element {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+        <Header />
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
