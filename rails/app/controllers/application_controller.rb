c5be485d2966e560610c0fa79af5f1a0ad9293f2
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-21 16:05:55 +0900

    コーディングルールに沿うよう前コミット分を修正

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::API
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+    end
 end
