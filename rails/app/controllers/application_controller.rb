9e003113635e9fe8635280a68edf0cf28298307f
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-21 16:02:51 +0900

    deviseサインアップ時のストロングパラメーターにnameを追加

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -3,4 +3,12 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include DeviseHackFakeSession
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+  end
 end
