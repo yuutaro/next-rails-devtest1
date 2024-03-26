496a239cf3c4c3ad842d12ff46b322f85b8408d8
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2024-03-26 14:38:33 +0900

    devise_token_auth 導入

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
   namespace :api do
     namespace :v1 do
       get 'health_check', to: 'health_check#index'
+      mount_devise_token_auth_for "User", at: "auth"
     end
   end
 end
