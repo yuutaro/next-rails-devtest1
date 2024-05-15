321eec9e2dfeba83ef212c35a60eb26071fac939
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2024-05-15 19:24:19 +0900

    N+1問題解消

--- rails/app/controllers/api/v1/articles_controller.rb
+++ rails/app/controllers/api/v1/articles_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::ArticlesController < Api::V1::BaseController
 
     def index
-      # 公開中の記事のレコードのみ且つ作成日順且つ1ページにつき10個取得
-     articles = Article.published.order(created_at: :desc).page(params[:page] || 1).per(10)
+      # 公開中の記事のレコードのみ且つ作成日順且つ1ページにつき10個取得かつUserレコードを全て取得
+     articles = Article.published.order(created_at: :desc).page(params[:page] || 1).per(10).includes(:user)
      render json: articles
     end
   
