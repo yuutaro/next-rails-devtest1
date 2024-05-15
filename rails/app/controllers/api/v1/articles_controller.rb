ea3404af9f4e5c8e925d2559caf38640561b760c
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2024-05-15 19:31:53 +0900

    ページ数の取得

--- rails/app/controllers/api/v1/articles_controller.rb
+++ rails/app/controllers/api/v1/articles_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::ArticlesController < Api::V1::BaseController
 
-    def index
-      # 公開中の記事のレコードのみ且つ作成日順且つ1ページにつき10個取得かつUserレコードを全て取得
-     articles = Article.published.order(created_at: :desc).page(params[:page] || 1).per(10).includes(:user)
-     render json: articles
-    end
+  #concernモジュールを読み込み、pagination(articles)でページ数を生成
+  include Pagination
+
+  def index
+    # 公開中の記事のレコードのみ且つ作成日順且つ1ページにつき10個取得かつUserレコードを全て取得
+    articles = Article.published.order(created_at: :desc).page(params[:page] || 1).per(10).includes(:user)
+    #ページ情報をmetaに仕込み、adapterでmetaをレスポンスjsonに統合
+    render json: articles, meta: pagination(articles), adapter: :json
+  end
   
   def show
     article = Article.published.find(params[:id])
