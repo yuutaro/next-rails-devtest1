5465b7abf3ca4bd524ec88b3481c8421d5dee012
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2024-05-15 20:46:29 +0900

    マイ記事一覧を取得するアクション　実装

--- rails/app/controllers/api/v1/current/articles_controller.rb
+++ rails/app/controllers/api/v1/current/articles_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::Current::ArticlesController < Api::V1::BaseController
   before_action :authenticate_user!
 
+  def index
+      #未保存以外の記事を取得
+      articles = current_user.articles.not_unsaved.order(created_at: :desc)
+      render json: articles
+  end
+
   def create
-    #.firstで現在のユーザーが保有している未保存記事を取得。nilでない場合、unsaved_articleに代入
+    # .firstで現在のユーザーが保有している未保存記事を取得。nilでない場合、unsaved_articleに代入
     unsaved_article = current_user.articles.unsaved.first || current_user.articles.create!(status: :unsaved)
     render json: unsaved_article
   end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
       namespace :current do
         resource :user, only: [:show]
-        resources :articles, only: [:create, :update]
+        resources :articles, only: [:index, :create, :update]
       end
       
       resources :articles, only: [:index, :show]
