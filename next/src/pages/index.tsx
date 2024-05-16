81fd217dc1912ba113fe6d6539fafa3e1144be8e
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2024-05-16 11:40:32 +0900

    モックアップ実装

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,19 +1,51 @@
+import { Box, Grid, Container } from '@mui/material'
+import camelcaseKeys from 'camelcase-keys'
 import type { NextPage } from 'next'
+import Link from 'next/link'
 import useSWR from 'swr'
+import ArticleCard from '@/components/ArticleCard'
 import { fetcher } from '@/utils'
 
-const Index: NextPage = () => {
-  const url = 'http://localhost:3000/api/v1/health_check'
-  const { data, error } = useSWR(url, fetcher)
+type ArticleProps = {
+  id: number
+  title: string
+  createdAt: string
+  fromToday: string
+  user: {
+    name: string
+  }
+}
 
+const Index: NextPage = () => {
+  // RailsAPIからarticlesレコードの取得
+  const url = 'http://localhost:3000/api/v1/articles'
+  //レコード取得にuseSWRとfetcherを使用。返ってきたbodyをdataに格納
+  const { data, error } = useSWR(url, fetcher)
   if (error) return <div>An error has occurred.</div>
   if (!data) return <div>Loading...</div>
 
+  console.log(data)
+  const articles = camelcaseKeys(data.articles)
+  //-------------------------//
+
   return (
-    <>
-      <div>Rails疎通確認</div>
-      <div>レスポンスメッセージ: {data.message}</div>
-    </>
+    <Box sx={{ backgroundColor: '#e6f2ff', minHeight: '100vh' }}>
+      <Container maxWidth="md" sx={{ pt: 6 }}>
+        <Grid container spacing={4}>
+          {articles.map((article: ArticleProps, i: number) => (
+            <Grid key={i} item xs={12} md={6}>
+              <Link href={'/articles/' + article.id}>
+                <ArticleCard
+                  title={article.title}
+                  fromToday={article.fromToday}
+                  userName={article.user.name}
+                />
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Box>
   )
 }
 
