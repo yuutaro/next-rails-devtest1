24b74665f9c73039cc27aa4fa6b7a28abad220eb
Ayumu1205 <ab20089@shibaura-it.ac.jp>
2024-05-16 12:18:46 +0900

    リセットCSS 適用

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import { AppProps } from 'next/app'
+import * as React from 'react'
+import '@/styles/destyle.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
